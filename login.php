ce53120ea16332316a858e3da79df19071bbcdde
CodePhysicist <tonchanin@hotmail.com>
2021-02-08 21:24:54 +0700

    Update
    Update

--- login.php
+++ login.php
@@ -225,7 +225,7 @@ Problem:
                             }
                             else if(response == "FL")
                             {
-                                ShowAlert("Max user limit reached. mac=".$mac);
+                                ShowAlert("Max user limit reached. mac="+$mac);
                                 return;
                             }
                             else if(response == "N")
